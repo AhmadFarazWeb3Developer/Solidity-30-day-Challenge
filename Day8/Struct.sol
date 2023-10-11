89446c86f27333feffa3b6cf0b8a80e01b8b1411
Ahmad Faraz <129995311+AhmadFarazWeb3Developer@users.noreply.github.com>
2023-10-11 21:25:19 +0500

    Update Struct.sol

--- Day8/Struct.sol
+++ Day8/Struct.sol
@@ -1,1 +1,32 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+contract AuthorDetails {
+    // Define a struct named Author
+    struct Author {
+        string Name;
+        string book;
+        string qualification;
+    }
+    Author author; // Declare a variable of type Author
+    // Constructor function to initialize author details
+    function authorDetails() public {
+        author = Author(
+            "Robert Green",
+            "48 Laws of Power",
+            "PHD in Philosophy"
+        );
+    }
+    // Function to get the author's name
+    function getName() public view returns (string memory) {
+        return author.Name;
+    }
+    // Function to get the author's book
+    function getBook() public view returns (string memory) {
+        return author.book;
+    }
+    // Function to get the author's qualification
+    function getQualification() public view returns (string memory) {
+        return author.qualification;
+    }
+}
