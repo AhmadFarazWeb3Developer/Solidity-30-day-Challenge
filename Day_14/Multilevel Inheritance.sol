b4a632208f3d52b0f9bb24d96052dbb92fe3cb96
Ahmad Faraz <129995311+AhmadFarazWeb3Developer@users.noreply.github.com>
2023-10-17 19:15:51 +0500

    Update Multilevel Inheritance.sol

--- Day_14/Multilevel Inheritance.sol
+++ Day_14/Multilevel Inheritance.sol
@@ -1,13 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
+// Contract A is the parent class.
 contract A {
+    // State variable representing a sum.
     uint256 Sum_A = 100;
 
+    // Function that returns a string.
     function funA() public pure returns (string memory) {
         return " A is the Parent Class";
     }
 
+    // Function to get the value of Sum_A.
     function get_SUM_A() public view returns (uint256) {
         return Sum_A;
     }
@@ -15,12 +19,15 @@ contract A {
 
 // Contract B inherits from contract A.
 contract B is A {
+    // State variable in contract B.
     uint256 Sum_B = Sum_A + 100;
 
+    // Function that returns a string.
     function funB() public pure returns (string memory) {
         return " B is the child of A Class  ";
     }
 
+    // Function to get the value of Sum_B.
     function get_SUM_B() public view returns (uint256) {
         return Sum_B;
     }
@@ -28,11 +35,14 @@ contract B is A {
 
 // Contract C inherits from contract B.
 contract C is B {
+    // Function that returns a string.
     function funC() public pure returns (string memory) {
         return " C is the child of B Class and A Class   ";
     }
 
+    // Function to get the value of Sum_C.
     function get_SUM_C() public view returns (uint256) {
+        // Calculate Sum_C using Sum_B and Sum_A.
         uint256 Sum_C = Sum_B + Sum_A + 100;
         return Sum_C;
     }
