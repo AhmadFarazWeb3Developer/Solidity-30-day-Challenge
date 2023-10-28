ca53894b1413a3e3693edf993e4a1de3b9d91bd6
Ahmad Faraz <129995311+AhmadFarazWeb3Developer@users.noreply.github.com>
2023-10-28 17:52:50 +0500

    Update Modifiers.sol

--- Day_25/Modifiers.sol
+++ Day_25/Modifiers.sol
@@ -1,1 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+contract ContractWithModifier {
+    struct Person {
+        string Name;
+        uint256 Id;
+    }
+
+    address public Owner;
+    mapping(address => Person) Details;
+
+    constructor() {
+        Owner = msg.sender;
+    }
+
+    modifier isOwner() {
+        require(msg.sender == Owner, " You are not the owner");
+        _;
+    }
+
+    // If the owner has been changed, then the isOwner modifier
+    // would be called and "You are not the owner" would be printed.
+    
+    function setOWnerDetails(string memory _name, uint256 _id) public isOwner {
+        Details[Owner].Name = _name;
+        Details[Owner].Id = _id;
+    }
+
+    // Only the owner can return the details
+    function getOwnerDetails() public view isOwner returns (string memory, uint256) {
+        return (Details[Owner].Name, Details[Owner].Id);
+    }
+}
 
