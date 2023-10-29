f7a66142c86996bb12c4d98d7a7c8256086050da
Ahmad Faraz <129995311+AhmadFarazWeb3Developer@users.noreply.github.com>
2023-10-29 18:50:41 +0500

    Update multipleOwnble.sol

--- Day_26/multipleOwnble.sol
+++ Day_26/multipleOwnble.sol
@@ -1,1 +1,30 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+contract multiOwnable {
+    mapping(address => bool) isOwner; // Track ownership status for each address.
+    address[] owners; // Store addresses of all owners.
+
+    constructor() {
+        owners.push(msg.sender);
+        isOwner[msg.sender] = true;
+    }
+
+    // Function to add a new owner. Only callable by current owners.
+    function addOwner(address additionalAddress) public onlyOwner {
+        owners.push(additionalAddress);
+        isOwner[additionalAddress] = true;
+    }
+
+    // Modifier allows only owners to execute the function.
+    modifier onlyOwner() {
+        require(isOwner[msg.sender], "You are not the owner");
+        _;
+    }
+
+    // Function to retrieve the list of all owners. Only callable by owners.
+    function getOwners() public view onlyOwner returns (address[] memory) {
+        return owners;
+    }
+}
