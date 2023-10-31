9067e3cf2ce7d6fe855a65e8099fa0038a36c75b
Ahmad Faraz <129995311+AhmadFarazWeb3Developer@users.noreply.github.com>
2023-10-31 19:22:13 +0500

    Update transferOwner.sol

--- Day_28/transferOwner.sol
+++ Day_28/transferOwner.sol
@@ -1,15 +1,19 @@
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.21;
-contract Owner {
+
+contract tranferOwner {
+
     struct ActualOwner {
         string name;
         string location;
     }
+
     struct NewOwner {
         string name;
         string location;
     }
+
     mapping(address => ActualOwner) public actualOwnerDetails;
     address owner;
     mapping(address => NewOwner) newOwnerDetails;
@@ -19,6 +23,7 @@ contract Owner {
         actualOwnerDetails[msg.sender].name = "Ahmad Faraz";
         actualOwnerDetails[msg.sender].location = "Pakistan";
     }
+
     modifier onlyOwner() {
         require(owner == msg.sender, "You are not the owner");
         _;
@@ -30,8 +35,10 @@ contract Owner {
         newOwnerDetails[newOwnerAddress].location = _location;
     }
 
-    function getNewOwnerDetails(address newOwnerAddress) public view returns (string memory, string memory) {
+    function getNewOwnerDetails(address newOwnerAddress) public view returns 
+     (string memory name, string memory location) {
         require(msg.sender == newOwnerAddress, "You are not the new owner");
         return (newOwnerDetails[newOwnerAddress].name, newOwnerDetails[newOwnerAddress].location);
     }
 }
+
