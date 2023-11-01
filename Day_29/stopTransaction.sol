8bba46ca8f7bad6abc242f73c48c7180b0839875
Ahmad Faraz <129995311+AhmadFarazWeb3Developer@users.noreply.github.com>
2023-11-01 17:34:02 +0500

    Update stopTransaction.sol

--- Day_29/stopTransaction.sol
+++ Day_29/stopTransaction.sol
@@ -1,1 +1,38 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+contract Ownable {
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+}
+
+contract Stoppable is Ownable {
+    bool public isStopped;
+    
+    constructor() {
+        isStopped = false;
+    }
+    
+    function stopTransaction() external onlyOwner {
+        isStopped = false;
+    }
+    
+    function startTransaction() external onlyOwner {
+        isStopped = true;
+    }
+    
+    function withdraw (uint256 amount) external view onlyOwner returns (uint256)
+    {
+        require(isStopped == true,"call startTansaction method before withdrawing amount");
+        return amount;
+    }
+}
